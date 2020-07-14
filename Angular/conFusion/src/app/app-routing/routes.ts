d2950dd674c268c589d490aa45e287dd52ea3854
Napat Traikityanukul <59090011@kmitl.ac.th>
2020-07-14 11:28:52 +0700

    promotion

---
+++ Angular/conFusion/src/app/app-routing/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { MenuComponent } from '../menu/menu.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContractComponent } from '../contract/contract.component';
+
+export const routes: Routes = [
+    {path: 'home' , component: HomeComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'contractus', component: ContractComponent},
+    {path: '' , redirectTo: '/home', pathMatch: 'full'}
+];
--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -8,19 +8,22 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContractComponent } from './contract/contract.component';
+
+
+import { DishService } from './services/dish.service';
+ 
+
 
 @NgModule({
   declarations: [
